b3170c72c50fd4d39bb4432f8b6d958a5fcf3ab4
F1get9 <159920570+F1get9@users.noreply.github.com>
2025-04-19 15:20:49 +0200

    Update Convolutional-variational_neural_network1.py

--- Convolutional-variational_neural_network1.py
+++ Convolutional-variational_neural_network1.py
@@ -192,8 +192,8 @@ for idx, layer in enumerate(model.encoder.layers):
     output_shape = layer_model.output_shape[1:]
     feature_dim = np.prod(layer_model.output_shape[1:])
     print(f"Feature Dimension: {feature_dim}")
-    if feature_dim == 4:
-        MEAN(feature_dim, batch_size, train_images, layer_model, train_categories, output_shape, selected_labels)
     type_of_network = "Convolutional-variational"
+    if feature_dim == 4:
+        MEAN(feature_dim, batch_size, train_images, layer_model, train_categories, output_shape, selected_labels, type_of_network)
     PCA(feature_dim, batch_size, train_images, layer_model, train_categories, output_shape, selected_labels, type_of_network)
     TSNE(feature_dim, batch_size, train_images, layer_model, train_categories, output_shape, selected_labels, type_of_network)
